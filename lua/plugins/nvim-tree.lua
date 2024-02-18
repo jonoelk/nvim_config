7ae69634680ed19195591f0bc9920346ed3823b4
jonoelk <jono.elkhoury@gmail.com>
2024-02-18 21:18:20 +1100

    more plugins

---
+++ lua/plugins/nvim-tree.lua
@@ -0,0 +1,35 @@
+return {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    lazy = true,
+    keys = {
+        { '<leader>ee', '<cmd>NvimTreeOpen<cr>' },
+    },
+    config = function()
+        local nvimtree = require("nvim-tree")
+        local api = require("nvim-tree.api")
+
+        vim.g.loaded_netrw = 1
+        vim.g.loaded_netrwPlugin = 1
+
+        nvimtree.setup({
+            actions = {
+                open_file = { quit_on_open = true, },
+            },
+            view = {
+                width = 40,
+                relativenumber = true,
+            },
+            git = {
+                ignore = false,
+            },
+            renderer = {
+                indent_markers = {
+                    enable = true,
+                }
+            },
+        })
+
+    	vim.keymap.set('n', '<leader>ee', api.tree.toggle)
+    end,
+}

9966834aaeba8bb92cb6661b8c90f7f94e4ae912
jonoelk <jono.elkhoury@gmail.com>
2024-02-24 17:40:54 +1100

    debugger

---
+++ lua/plugins/nvim-dap.lua
@@ -0,0 +1,27 @@
+return {
+    "mfussenegger/nvim-dap",
+    config = function()
+        local dap = require("dap")
+        dap.adapters.gdb = {
+            type = "executable",
+            command = "gdb",
+            args = { "-i", "dap" }
+        }
+        dap.configurations.cpp = {
+            {
+                name = "Launch",
+                type = "gdb",
+                request = "launch",
+                program = function()
+                    return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+                end,
+                cwd = "${workspaceFolder}",
+                stopAtBeginningOfMainSubprogram = false,
+            },
+        }
+
+        vim.keymap.set('n', '<leader>dl', function() require('dap').run_last() end)
+        vim.keymap.set('n', '<leader>b', function() require('dap').toggle_breakpoint() end)
+        vim.keymap.set('n', '<leader>dc', function() require('dap').continue() end)
+    end,
+}

91602b8a56fa75e5c12281feea4ce9ae8aee4199
Jonathan El-Khoury <jonathan.elkhoury@vivcourt.com>
2024-03-14 17:02:42 +1100

    add find string

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -14,6 +14,7 @@ return {
         vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
         vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
         vim.keymap.set('n', '<leader>fs', builtin.live_grep, {})
+        vim.keymap.set('n', '<leader>fw', builtin.grep_string, {})
         vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
         vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
         vim.keymap.set('n', '<leader>fr', builtin.resume, {})

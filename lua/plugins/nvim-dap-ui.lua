70684b0375d6ffea9e4d3d2db4547299b23e254c
Jonathan El-Khoury <jonathan.elkhoury@vivcourt.com>
2024-04-18 08:45:57 +1000

    remove dap - using gdb instead

--- lua/plugins/nvim-dap-ui.lua
+++
@@ -1,8 +0,0 @@
-return {
-    "rcarriga/nvim-dap-ui",
-    config = function()
-        require("dapui").setup()
-
-        vim.keymap.set('n', '<leader>du', function() require("dapui").toggle() end)
-    end,
-}

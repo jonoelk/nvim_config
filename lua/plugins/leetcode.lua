9414a69ab140c358b799be55102e490542ac0b54
Jonathan El-Khoury <jonathan.elkhoury@vivcourt.com>
2024-07-01 13:09:13 +1000

    add lc plugin

---
+++ lua/plugins/leetcode.lua
@@ -0,0 +1,19 @@
+local leet_arg = "leetcode"
+return {
+    "kawre/leetcode.nvim",
+    build = ":TSUpdate html",
+    lazy = leet_arg ~= vim.fn.argv()[1],
+    dependencies = {
+        "nvim-telescope/telescope.nvim",
+        "nvim-lua/plenary.nvim", -- required by telescope
+        "MunifTanjim/nui.nvim",
+
+        -- optional
+        "nvim-treesitter/nvim-treesitter",
+        "rcarriga/nvim-notify",
+        "nvim-tree/nvim-web-devicons",
+    },
+    opts = {
+        arg = leet_arg
+    },
+}
